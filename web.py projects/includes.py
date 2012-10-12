af53c4d69001e3e4442b68481ff4ec848102bd1c
chewxy <chewxy@gmail.com>
2012-10-13 04:53:17 +1100

    Converted indentations to spaces.

--- web.py projects/includes.py
+++ web.py projects/includes.py
@@ -13,19 +13,24 @@ import ConfigParser
 import json
 import redis
 
-from redisSessions import redisSessions
+# Render Engines! 
+import tenjin
+from tenjin.helpers import *
+
+# redis session
+import redisSessions
 
 #config and messages
 config = ConfigParser.SafeConfigParser()
 config.read('config.ini')
 
-web.config.debug = True
+web.config.debug = True  # set this to false when deploying (i.e. Master branch commits)
 
 # Routes
 urls = (
-		# static pages get listed first
-		'/', 'index',
-		)
+        # static pages get listed first
+        '/', 'index',
+        )
 
 curdir = os.path.dirname(__file__)
 
@@ -37,13 +42,13 @@ redisServer = redis.StrictRedis()  # fill in details!
 
 #Session stuff
 session = web.session.Session(app,
-							redisSessions.RedisStore(ip='xxx.xxx.xxx.xxx',  # if you don't know IP addresses, shame on you
-													port=1234,  # port has to be integer
-													db=1,  # redis database ID, has to be integer
-													initialFlush=False),  # when deploying set initialFlush to true
-													initializer={},  # whatever the fuck you want to initialize
-													format='Default'  # choose 'json' if you want to mess around. 
-													)
+                            redisSessions.RedisStore(ip='xxx.xxx.xxx.xxx',  # if you don't know IP addresses, shame on you
+                                                    port=1234,  # port has to be integer
+                                                    db=1,  # redis database ID, has to be integer
+                                                    initialFlush=False),  # when deploying set initialFlush to true
+                                                    initializer={},  # whatever the fuck you want to initialize
+                                                    format='Default'  # choose 'json' if you want to mess around. 
+                                                    )
 
 # these are the default settings. Think you might want to set this up
 web.config.session_parameters = web.storify({
@@ -59,6 +64,16 @@ web.config.session_parameters = web.storify({
     'secure': False
 })
 
-# Declare your database shit. Also, make up your bloody mind. Stop switchiing from MySQL to CouchDB to Neo4j for no good reason
+# Declare your database shit. Also, make up your bloody mind, Chewxy. 
+# Stop switchiing from MySQL to CouchDB to Neo4j for no good reason.
+
+
+# Render Engine (tenjin rhymes with engine):
+render = web.template.render('templates/', globals={
+                                                    'messages': messages,
+                                                    },
+                                cache=False
+                            )  # Templator (slow as hell)
+render = tenjin.Engine(path=['templates'])  # tenjin
 
 from views import *
